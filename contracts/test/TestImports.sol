f21f52bea3f2b2993ee7926e909fc3ed0b4069d9
Sem <931684+sembrestels@users.noreply.github.com>
2022-08-28 18:31:22 +0200

    Migrate to buidler/hardhat

--- contracts/test/TestImports.sol
+++ contracts/test/TestImports.sol
@@ -4,9 +4,8 @@ import "@aragon/os/contracts/acl/ACL.sol";
 import "@aragon/os/contracts/kernel/Kernel.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
 import "@aragon/os/contracts/factory/DAOFactory.sol";
-import "@aragon/apps-shared-migrations/contracts/Migrations.sol";
 
-import "./mocks/ERC20Sample.sol";
+// import "./mocks/ERC20Sample.sol";
 
 
 // HACK to workaround truffle artifact loading on dependencies
---
+++ hardhat.config.js
@@ -0,0 +1,2 @@
+require("@nomiclabs/hardhat-truffle5");
+module.exports = require('@aragon/hardhat-config')
